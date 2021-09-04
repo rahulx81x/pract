e2af6e56ff22cf489109cdd749af4aa0312fb485
rahulx81x <rahulgouri072@gmail.com>
2021-09-04 20:31:12 +0530

    regular expression assignment //Coursera-access webdata using python

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-
-Ed = [1,2,3,4,5]
-Jo = [5,4,3,2,1]
-edLimit = 0
-joLimit = 5
-
-
-def calc(ed, jo, edlimit, jolimit):
-    savings = 0
-    combi = sorted(zip(ed, jo), reverse=True)
-    for i in combi:
-
-        if i[0] >= i[1] and edlimit > 0:
-            edlimit += -1
-            savings += i[0]
-        elif jolimit > 0:
-            jolimit += -1
-            savings += i[1]
-        else:
-            continue
-    return savings
-
-
-print(calc(Ed, Jo, edLimit, joLimit))
+#regular expression assignment
+import re
+actual = open("actual.txt", "r")
+summation = 0
+for line in actual:
+    list_of_int = re.findall('[0-9]+', line)
+    list_of_int = list(map(int, list_of_int))
+    for integer in list_of_int:
+        summation += integer
+print(summation)
